c7d3d95b3799fcb319c67843a4c3c36d062efdf2
sheyleenh <sheyhc1005@gmail.com>
2024-12-05 21:07:07 -0500

    add learningpath section

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,3 +3,4 @@ export { default as AboutMe } from './AboutMe';
 export { default as Skills } from './Skills';
 export { default as Projects } from './Projects';
 export { default as ContactMe } from './ContactMe';
+export { default as LearningPath } from './LearningPath';
